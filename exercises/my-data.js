7c58196c0394185791c8934d481e6990e3bc9dbe
6levi9 <robertojr.rga@gmail.com>
2023-04-25 00:32:04 -0400

    Mis datos

---
+++ exercises/my-data.js
@@ -0,0 +1,13 @@
+const myData = {
+    "Name" : "Levi",
+    "Year" : 19,
+    "Are you a developer?": true,
+    "birthdate": new Date(23,08,2001),
+    "favorite books":  books = {
+        "title": "1984",
+        "autor": "George Orwell",
+        "Date": "8 de junio de 1949" ,
+        "url": "https://es.wikipedia.org/wiki/1984_(novela)"
+
+    }
+}
